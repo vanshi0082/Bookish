9991bb39cf17def09c70d636e54abc18c29c01c1
Vanshi Rajput <115893209+vanshi0082@users.noreply.github.com>
2025-07-09 00:08:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://book-app-frontend-tau.vercel.app'],
+    origin: ['http://localhost:5173', 'https://bookish-us45.onrender.com'],
     credentials: true
 }));
 
